625d1117424d63de57a9d4988bf4e103ce3566f2
unknown <hhcarpenter12@gmail.com>
2019-11-28 10:49:22 -0600

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,23 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    imports: [
+      RouterModule.forRoot(
+        appRoutes)
+      // other imports here
+    ],
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: '/my-swim-workout',
+    pathMatch: 'full'
+  }
+];
+
 export class AppModule { }
